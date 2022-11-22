56488d7148810dd2876d91e5a6e712d79f62922b
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-22 22:16:55 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
    int n=0, a=1, b=0, c=1;
     cin>>n;
-    if (n>=2) for (int i=0; i<n; i++)
+    if (n>=2) for (int i=0; i<(n/3); i++)
     {
         c=a+b;
         cout<<c<<endl;

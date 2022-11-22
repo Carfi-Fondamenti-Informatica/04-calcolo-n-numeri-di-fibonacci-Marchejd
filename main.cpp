8a59ce4682c426be568b18a86188776875fcc496
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-22 22:08:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 int main() {
+   int n=0, a=1, b=0, c=1;
+    cin>>n;
+    if (n>=2) for (int i=0; i<n; i++)
+    {
+        c=a+b;
+        cout<<c<<endl;
+        a=c+b;
+        cout<<a<<endl;
+        b=a+c;
+        cout<<b<<endl;
+    }
    return 0;
 }

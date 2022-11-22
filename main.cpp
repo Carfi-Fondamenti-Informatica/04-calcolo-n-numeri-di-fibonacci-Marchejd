db0f0f317e9d8ee438851db29909f401d44c9b96
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-22 22:41:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
    int n=0, a=1, b=1, c=0;
     cin>>n;
-    if (n<2)  {cout<<"errore"<<ednl;}
+    if (n<2)  {cout<<"errore"<<endl;}
    else { int i=0;
-          cout<<a<<ednl;
-          cout<<b<<ednl;
+          cout<<a<<endl;
+          cout<<b<<endl;
           for (i=3; i<=n; i++)
           { c=a+b;
             cout<<c<<endl;

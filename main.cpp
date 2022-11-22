f9f435371008264894d71298b28face1997e6a22
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-22 22:26:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int main() {
    int n=0, a=1, b=0, c=1;
     cin>>n;
-    if (n>=2) for (int i=0; i<n; i++)
-    {
-            c=a+b;
+    if (n>=2)
+    {for (int i=0; i<n; i++)
+    {       c=a+b;
             cout<< c << endl;
             a=b;
             b=c;
-
     }
+    }
+   else {cout<<"errore"<<ednl;}
    return 0;
 }
